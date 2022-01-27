0f8b9b127a21b0ee6e2fc8b14da336b5c38cf432
Ksenia Baranova <baranova-ksenia@mail.ru>
2022-01-27 11:26:56 +0200

    components created

---
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -0,0 +1,25 @@
+﻿import React from "react";
+import styles from "./MealItemForm.module.css";
+
+import Input from "./../../UI/Input";
+
+function MealUtemForm(props) {
+  return (
+    <form className={styles.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          max: "5",
+          min: "1",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
+}
+
+export default MealUtemForm;
---
+++ src/components/UI/Input.jsx
@@ -0,0 +1,15 @@
+﻿import React from "react";
+
+import styles from "./Input.module.css";
+function Input(props) {
+  return (
+    <div className={styles.input}>
+      <label htmlFor={props.input.id} className={styles.label}>
+        {props.label}
+      </label>
+      <input {...props.input} />
+    </div>
+  );
+}
+
+export default Input;
